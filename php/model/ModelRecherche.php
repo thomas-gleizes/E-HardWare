80ac23724652d8a39fbbdd05a260e260418f1b88
viatord <socialempire971@hotmail.fr>
2019-11-15 11:47:04 +0100

    recherche

--- php/model/ModelRecherche.php
+++ php/model/ModelRecherche.php
@@ -4,13 +4,13 @@ require_once ('../model/Model.php');
 class ModelRecherche{
 
     public static function afficherRecherche($val){
-        $sql = "SELECT * FROM Produits where nom like '%':val'l%'";
+        $sql = "SELECT refProduit FROM Produits where nom like '%':val'l%'";
         $valeur  =array(
             "val" => $val
         );
         $rec_prep = Model::$pdo->prepare($sql);
         $rec_prep->execute($valeur);
-        $rec_prep->setFecthMode(PDO::FETCH_COLUMN,1);
+        $rec_prep->setFecthMode(PDO::FETCH_COLUMN,0);
         $tab = $rec_prep->fetchAll();
         if(empty($tab))
             return null;
