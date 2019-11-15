3193404438a67cbddeab0af75557daa58f3630fe
viatord <socialempire971@hotmail.fr>
2019-11-15 12:00:23 +0100

    visu

--- php/controller/ControllerRecherche.php
+++ php/controller/ControllerRecherche.php
@@ -3,6 +3,7 @@
 require_once ("../model/ModelRecherche.php");
 class ControllerRecherche{
     public static function afficherRecherch(){
+        var_dump($_POST);
         $tab = ModelRecherche::afficherRecherche($_POST);
         $string = implode($tab);
         header("");
