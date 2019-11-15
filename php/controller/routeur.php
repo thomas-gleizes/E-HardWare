7a407ea72d9d4816901c5366f6815a0feae7552b
viatord <socialempire971@hotmail.fr>
2019-11-15 11:57:45 +0100

    maj mineur controller

--- php/controller/routeur.php
+++ php/controller/routeur.php
@@ -1,5 +1,5 @@
 <?php
-    require_once ('ControllerVoiture.php');
+    require_once ('ControllerRecherche.php');
     if(!$_GET==null){
         $action = $_GET["action"];
         controller::$action();
