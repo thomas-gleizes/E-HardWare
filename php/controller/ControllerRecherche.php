cfc7bbac85ca0e414a8c478a1f160e70c710ea15
basile-savouret <basile.savouret@etu.umontpellier.fr>
2019-11-15 13:29:34 +0100

    travail

--- php/controller/ControllerRecherche.php
+++ php/controller/ControllerRecherche.php
@@ -2,7 +2,7 @@
 
 require_once ("../model/ModelRecherche.php");
 class ControllerRecherche{
-    public static function afficherRecherch(){
+    public static function afficherRecherche(){
         $tab = ModelRecherche::afficherRecherche($_POST);
         $string = implode($tab);
         header("");
--- php/controller/routeur.php
+++ php/controller/routeur.php
@@ -2,6 +2,6 @@
     require_once ('ControllerRecherche.php');
     if(!$_GET==null){
         $action = $_GET["action"];
-        controller::$action();
+        ControllerRecherche::$action();
     }
 ?>
