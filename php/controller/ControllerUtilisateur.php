8ab5e3f1158d7386e913caadd78f5defae748549
Thomas Gleizes <thomas.gleizes@etu.umontpellier.fr>
2019-11-15 16:00:00 +0100

    controller model

--- php/controller/ControllerUtilisateur.php
+++ php/controller/ControllerUtilisateur.php
@@ -7,17 +7,19 @@ class ControllerUtilisateur{
     public static function creation(){
         $tab = [];
         $tab['email'] = $_POST['mail'];
+        $tab['nom'] = $_POST['nom'];
+        $tab['prenom'] = $_POST['prenom'];
+        $tab['mdp'] = $_POST['mdp'];
+        $tab['adresse'] = $_POST['adresse'];
 
         ModelUtilisateur::creationCompte($tab);
-
-        require_once ("../view/vueRecherche.php");
+        header('Location:../view/vueRecherche.php');
     }
 
     public static function connection(){
         $tab = [];
         $tab['email'] = $_POST['mail'];
-        $tab['mot de passe'] = ModelUtilisateur::['mot de passe'];
-
+        $tab['mot de passe'] = $_POST['mot de passe'];
 
         ModelUtilisateur::connectionCompte($tab);
         require_once ("../view/vueRecherche.php");
