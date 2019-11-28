16fae55662d7210ccc211a496b41adba7b5ad7c6
thomas <thomas.gleizes@etu.umontpellier.fr>
2019-11-28 16:02:31 +0100

    Form Ajout de produit

--- php/view/vueRecherche.php
+++ php/view/vueRecherche.php
@@ -280,6 +280,45 @@ if (isset($_SESSION['admin'])){
                 <input type="url" class="ajout-input" placeholder="Url de l\'image du produit" name="url" required>
                 <button id="ok" type="submit"><p>Ajout</p></button>
             </form>
+            <<form method="post" action="." class="Processeur">
+                <input type="text" class="ajout-input" placeholder="Nombre de coeur" name="nbCoeur" required>
+                <input type="text" class="ajout-input" placeholder="Nombre de threads" name="nbThreads" required>
+                <input type="text" class="ajout-input" placeholder="Socket" name="socket" required>
+                <input type="text" class="ajout-input" placeholder="Frequence CPU" name="frequence" required>
+                <input type="text" class="ajout-input" placeholder="Boost CPU" name="Boost" required>
+                <input type="text" class="ajout-input" placeholder="Cache" name="cache" required>
+            </form method="post" action="" class="CarteGraphique">
+                <input type="text" class="ajout-input" placeholder="Chipset Graphique" name="chipset" required>
+                <input type="text" class="ajout-input" placeholder="Mémoire vidéo" name="memoire" required>
+                <input type="text" class="ajout-input" placeholder="Architecture" name="architecture" required>
+                <input type="text" class="ajout-input" placeholder="Bus" name="bus" required>
+            <form>
+            
+</form>
+            <form method="post" action="" class="CarteMere">
+                <input type="text" class="ajout-input" placeholder="Chipset" name="chipset" required>
+                <input type="text" class="ajout-input" placeholder="Socket" name="socket" required>
+                <input type="text" class="ajout-input" placeholder="Format" name="format" required>
+            </form>
+            <form method="post" action="" class="DisqueDur">
+                <input type="text" class="ajout-input" placeholder="Capacite HDD" name="capacite" required>
+                <input type="text" class="ajout-input" placeholder="Interface" name="interface" required>
+                <input type="text" class="ajout-input" placeholder="Vitesse de rotation" name="vitesseRotation" required>
+            </form>
+            <form method="post" action="" class="Mémoire">
+                <input type="text" class="ajout-input" placeholder="Type de RAM" name="type" required>
+                <input type="text" class="ajout-input" placeholder="Capacite RAM" name="capacite" required>
+                <input type="text" class="ajout-input" placeholder="Frequence RAM" name="frequence" required>
+                <input type="text" class="ajout-input" placeholder="CAS" name="CAS" required>
+                <input type="text" class="ajout-input" placeholder="Nombre de barrrette" name="nbBarrette" required>            
+            </form>
+            <form method="post" action="" class="SSD">
+                <input type="text" class="ajout-input" placeholder="Format SSD" name="format" required>
+                <input type="text" class="ajout-input" placeholder="Capacite SSD" name="capacite" required>
+                <input type="text" class="ajout-input" placeholder="Interface SSD" name="interface" required>
+                <input type="text" class="ajout-input" placeholder="Vitesse de lecture" name="lecture" required>
+                <input type="text" class="ajout-input" placeholder="Vitesse d\'écriture" name="ecriture" required>
+           </form>
         </div>
         ';
     }
