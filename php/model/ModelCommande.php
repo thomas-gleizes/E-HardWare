fe334192dee1367425af1c5b5cadd31a04843049
thomas <thomas.gleizes@etu.umontpellier.fr>
2019-11-29 18:48:14 +0100

    addOrder ModelCommande

--- php/model/ModelCommande.php
+++ php/model/ModelCommande.php
@@ -3,13 +3,24 @@ require_once ('../model/Model.php');
 
 class ModelCommande{
 
-    public static function CreateOrder ($tab){
+    public static function CreateOrder($tab){
         $sql = "INSERT INTO Commandes VALUES ('', :date, :idClient, '', '', 'En attente')";
         $value = array(
             "date" => $tab['date'],
             "idClient" => $tab['idClient'],
         );
         $rec_prep = Model::$pdo->prepare($sql);
+        $rec_prep->execute($value);
+    }
+
+    public static function addOrder($tab){
+        $sql = "INSERT INTO ListeCommander VALUES (:idCommande, :refProduit, :quantite)";
+        $value = array(
+            "idCommande" => $tab['idCommande'],
+            'refProduit' => $tab['refProduit'],
+            'quantite' => $tab['quantite'],
+        );
+        $rec_prep = Model::$pdo->preapre($sql);
         $rec_prep->execute($value);
     }
 
