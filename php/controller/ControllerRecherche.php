c0a2356d40bdc756cd1582f8f1147a1dde6830ac
Yanis EL YOUSSOUFI <yanis.el-youssoufi@etu.umontpellier.fr>
2019-12-06 13:49:08 +0100

    Merge branch 'master' of /home/ann2/elyoussoufiy/ProjetPHP with conflicts.

--- php/controller/ControllerRecherche.php
+++ php/controller/ControllerRecherche.php
@@ -3,21 +3,8 @@
 require_once ("../model/ModelRecherche.php");
 class ControllerRecherche{
     public static function afficherRecherche(){
-        //var_dump($_GET);
-        $tab = ModelRecherche::afficheRechercheComplexe($_GET["research"],$_GET["prix"],$_GET["marque"],$_GET["categorie"]);
-        $tabvaleur = ModelRecherche::getAllinfo($tab);
-        //var_dump($tabvaleur);
-        require_once ("../view/vueRecherche.php");
-    }
-
-    public static function rechercheVide(){
-        $tabvaleur = [];
-        require_once ("../view/vueRecherche.php");
-    }
-
-    public static function rechercherSidebar(){
-        $tab = ModelRecherche::infoSidebar($_GET["categorie"]);
-        $tabvaleur = ModelRecherche::getAllInfo($tab);
-        require_once ("../view/vueRecherche.php");
+        $tab = ModelRecherche::afficherRecherche($_POST);
+        $string = implode($tab);
+        header("");
     }
 }
